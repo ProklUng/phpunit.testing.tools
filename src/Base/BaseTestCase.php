c487c6678033f95a00295fee367f05f42acba297
ProklUng <funtik21975@gmail.com>
2021-06-15 09:46:04 +0300

    BaseTestCase. Доработки.

--- src/Base/BaseTestCase.php
+++ src/Base/BaseTestCase.php
@@ -13,6 +13,8 @@ use Prokl\TestingTools\Traits\DataProvidersTrait;
 use Prokl\TestingTools\Traits\ExceptionAsserts;
 use Prokl\TestingTools\Traits\LoaderTestContainerTrait;
 use Prokl\TestingTools\Traits\PHPUnitTrait;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
 use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -51,6 +53,11 @@ class BaseTestCase extends TestCase
      * @var PHPMockerFunctions $mockerFunctions Мокер функций.
      */
     protected $mockerFunctions;
+
+    /**
+     * @var array $matched_dirs
+     */
+    private $matched_dirs = [];
 
     /**
      * @inheritDoc
@@ -112,6 +119,99 @@ class BaseTestCase extends TestCase
         Mockery::close();
         // Сбросить замоканные функции.
         $this->mockerFunctions->shutdown();
+    }
+
+    /**
+     * Creates a unique temporary file name.
+     *
+     * The directory in which the file is created depends on the environment configuration.
+     *
+     * @return string|boolean Path on success, else false.
+     */
+    public function tempFilename()
+    {
+        $tmp_dir = '';
+        $dirs = ['TMP', 'TMPDIR', 'TEMP'];
+
+        foreach ($dirs as $dir) {
+            if (isset($_ENV[$dir]) && !empty($_ENV[$dir])) {
+                $tmp_dir = $dir;
+                break;
+            }
+        }
+
+        if (empty($tmp_dir)) {
+            $tmp_dir = sys_get_temp_dir();
+        }
+
+        $tmp_dir = realpath($tmp_dir);
+
+        return tempnam($tmp_dir, 'wpunit');
+    }
+
+    /**
+     * Returns a list of all files contained inside a directory.
+     *
+     * @param string $dir Path to the directory to scan.
+     *
+     * @return array List of file paths.
+     */
+    protected function filesInDir(string $dir): array
+    {
+        $files = [];
+
+        $iterator = new RecursiveDirectoryIterator($dir);
+        $objects = new RecursiveIteratorIterator($iterator);
+        foreach ($objects as $name => $object) {
+            if (is_file($name)) {
+                $files[] = $name;
+            }
+        }
+
+        return $files;
+    }
+
+    /**
+     * Deletes all directories contained inside a directory.
+     *
+     * @param string $path Path to the directory to scan.
+     *
+     * @return void
+     *
+     */
+    protected function deleteFolders(string $path) : void
+    {
+        $this->matched_dirs = [];
+        if (!is_dir($path)) {
+            return;
+        }
+
+        $this->scandir($path);
+        foreach (array_reverse($this->matched_dirs) as $dir) {
+            rmdir($dir);
+        }
+
+        rmdir($path);
+    }
+
+    /**
+     * Retrieves all directories contained inside a directory and stores them in the `$matched_dirs` property. Hidden
+     * directories are ignored.
+     *
+     * This is a helper for the `delete_folders()` method.
+     *
+     * @param string $dir Path to the directory to scan.
+     *
+     * @return void
+     */
+    protected function scandir(string $dir) : void
+    {
+        foreach (scandir($dir) as $path) {
+            if (0 !== strpos($path, '.') && is_dir($dir . '/' . $path)) {
+                $this->matched_dirs[] = $dir . '/' . $path;
+                $this->scandir($dir . '/' . $path);
+            }
+        }
     }
 
     /**
